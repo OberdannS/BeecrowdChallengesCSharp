a30ad2aa8dc43b603bb5f59499e24fa6eb59e30c
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 01:00:08 -0300

    Bee1015

---
+++ Bee1015/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bee1015;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] x = Console.ReadLine().Split(' ');
+        string[] y = Console.ReadLine().Split(' ');
+        double x1 = double.Parse(x[0]);
+        double y1 = double.Parse(x[1]);
+        double x2 = double.Parse(y[0]);
+        double y2 = double.Parse(y[1]);
+        double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        Console.WriteLine(distancia.ToString("F4"));
+    }
+}
