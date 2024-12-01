35fd62b8b1b571e06e53f2098ae27761d03efad2
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:59:37 -0300

    Bee1014

---
+++ Bee1014/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bee1014;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int km = int.Parse(Console.ReadLine());
+        double litros = double.Parse(Console.ReadLine());
+        double kml = km / litros;
+        Console.WriteLine(kml.ToString("F3") + " km/l");
+    }
+}
