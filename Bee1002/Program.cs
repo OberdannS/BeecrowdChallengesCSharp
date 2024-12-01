c50bb9c979b8aefb5048496851f6775a1bcf3460
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:50:55 -0300

    Bee1002

---
+++ Bee1002/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bee1002;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double raio = double.Parse(Console.ReadLine());
+        double n = 3.14159;
+        double area = Math.Pow(raio, 2) * n;
+        Console.WriteLine("A=" + area.ToString("F4"));
+    }
+}
