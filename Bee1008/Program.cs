eeafd5f6b2ef15fc2eaa3ca85b0368c9764f1204
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:56:17 -0300

    Bee1008

---
+++ Bee1008/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bee1008;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+        double valueHours = double.Parse(Console.ReadLine());
+        double salary = hours * valueHours;
+        Console.WriteLine("NUMBER = {0}", number);
+        Console.WriteLine("SALARY = U$ {0}", salary.ToString("F2"));
+    }
+}
