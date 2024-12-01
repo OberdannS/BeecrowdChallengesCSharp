f7259bbcbd6c1359cf152d27848eca94c19cde7d
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:56:52 -0300

    Bee1009

---
+++ Bee1009/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Bee1009;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = Console.ReadLine();
+        double salario = double.Parse(Console.ReadLine());
+        double vendas = double.Parse(Console.ReadLine());
+        double total = (vendas * 15 / 100) + salario;
+        Console.WriteLine("TOTAL = R$ " + total.ToString("F2"));
+    }
+}
