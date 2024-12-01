1aa187cf241c9021a4cbe2175371f3bf73d8719c
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:48:22 -0300

    Bee1001

---
+++ Bee1001/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bee1001;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int x = a + b;
+        Console.WriteLine("X = {0}", x);
+    }
+}
