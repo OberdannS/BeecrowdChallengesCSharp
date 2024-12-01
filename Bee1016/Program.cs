29994bd393f188817f69b3aaad4ae4434a154c89
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 12:26:17 -0300

    Bee1016

---
+++ Bee1016/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Bee1016;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        n *= 2;
+        Console.WriteLine($"{n} minutos");
+    }
+}
