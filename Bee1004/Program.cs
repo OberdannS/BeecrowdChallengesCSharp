efe30ce80e1be277d3430ca0a76ef84d9b4d7da2
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:53:05 -0300

    Bee1004

---
+++ Bee1004/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bee1004;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int prod = a * b;
+        Console.WriteLine("PROD = " + prod);
+    }
+}
