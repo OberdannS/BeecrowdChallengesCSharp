bf6058c6bf05e799a4dfe7eb0ab759c17b4f14ae
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:54:41 -0300

    Bee1006

---
+++ Bee1006/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bee1006;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        a *= 2;
+        b *= 3;
+        c *= 5;
+        double media = (a + b + c) / 10;
+        Console.WriteLine("MEDIA = " + media.ToString("F1"));
+    }
+}
