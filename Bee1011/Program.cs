91b9be411c78a45f87a9486d3281306355bbc27f
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 00:58:08 -0300

    Bee1011

---
+++ Bee1011/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Bee1011;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int raio = int.Parse(Console.ReadLine());
+        double volume = (((double) 4 / 3) * 3.14159) * Math.Pow(raio, 3);
+        Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+    }
+}
