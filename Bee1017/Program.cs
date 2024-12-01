7e1b95cb83dbf7fb8066c0c935ff0d86d85c63aa
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-01 12:17:54 -0300

    Bee1017

---
+++ Bee1017/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Bee1017;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tempo = int.Parse(Console.ReadLine());
+        int velocidade = int.Parse(Console.ReadLine());
+        int distancia = tempo * velocidade;
+        double kml = (double) distancia / 12;
+        Console.WriteLine(kml.ToString("F3"));
+    }
+}
