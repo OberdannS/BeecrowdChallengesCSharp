0dd12e141e3246d63617ab62aac5c38ef7a81a64
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-02 23:30:48 -0300

    Bee1019

---
+++ Bee1019/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Bee1019;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int h, m, s;
+        h = n / 3600;
+        m = (n % 3600) / 60;
+        s = (n % 3600) % 60;
+        Console.WriteLine($"{h}:{m}:{s}");
+    }
+}
