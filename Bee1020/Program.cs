06e4302d7ed65d3aeead929b8355487647dd3794
Oberdan Santos <oberdanjsantos@gmail.com>
2024-12-02 23:36:58 -0300

    Bee1020

---
+++ Bee1020/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Bee1020;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dias = int.Parse(Console.ReadLine());
+        int ano, mes, dia;
+        ano = dias / 365;
+        mes = (dias % 365) / 30;
+        dia = (dias % 365) % 30;
+        Console.WriteLine($"{ano} ano(s)");
+        Console.WriteLine($"{mes} mes(es)");
+        Console.WriteLine($"{dia} dia(s)");
+    }
+}
